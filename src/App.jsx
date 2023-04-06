97d390a2c686bafec1697a9515e60bb8c8512e85
sahebbali <sahebbali253@gmail.comm>
2023-04-06 09:43:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Calculetor from './Pages/Calculetor/Calculetor'
+
+function App() {
+
+  return (
+    <div>
+      <Calculetor/>
+    </div>
+  )
+}
+
+export default App
